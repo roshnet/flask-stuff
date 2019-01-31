935bec6824c94035a526e57c7dd412cc8703c8aa
Roshan Sharma <reprogram46@gmail.com>
2019-02-01 02:41:56 +0530

    Demonstrate flask-login

---
+++ demos/flask-login/app.py
@@ -0,0 +1,65 @@
+# Flask Login Demo..
+
+from flask import Flask
+from flask import session, url_for, redirect
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+
+app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = ''
+app.config['SECRET_KEY'] = 'some-secret-key'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User(user_id)
+
+
+class User(UserMixin):
+	def __init__(self, id):
+		self.id = id
+
+@app.route('/')
+def index():
+	return '''
+		<a href="/login/">Login</a>
+		<br>
+		<a href="/protected/">Protected</a>
+		<br>
+		<a href="/logout/">Log Out</a>
+		<br>
+	'''
+
+
+@app.route('/protected/')
+@login_required
+def protected():
+	return 'Protected Content'
+
+
+
+@app.route('/login/')
+def login():
+	login_user(User(1))
+	return 'Logged In'
+
+
+@app.route('/logout/')
+def logout():
+	logout_user()
+	return 'Logged Out'
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+
+
+
+
+
+
+
