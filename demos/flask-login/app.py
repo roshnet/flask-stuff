bde7adfb0b0d311c6f8fa3cef87fff989ed1096e
Roshan Sharma <reprogram46@gmail.com>
2019-02-02 19:00:21 +0530

    Lint (pep8)

--- demos/flask-login/app.py
+++ demos/flask-login/app.py
@@ -2,7 +2,13 @@
 
 from flask import Flask
 from flask import session, url_for, redirect
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    logout_user,
+    login_required
+)
 
 app = Flask(__name__)
 
@@ -14,57 +20,48 @@ app.config['SECRET_KEY'] = 'some-secret-key'
 
 @login_manager.user_loader
 def load_user(user_id):
-	return User(user_id)
+    return User(user_id)
 
 
 class User(UserMixin):
-	def __init__(self, id):
-		self.id = id
+    def __init__(self, id):
+        self.id = id
 
 
 @app.route('/')
 def index():
-	return '''
-		<a href="/login/">Login</a>
-		<br>
-		<a href="/protected/">Protected</a>
-		<br>
-		<a href="/logout/">Log Out</a>
-		<br>
-	'''
+    return '''
+        <a href="/login/">Login</a>
+        <br>
+        <a href="/protected/">Protected</a>
+        <br>
+        <a href="/logout/">Log Out</a>
+        <br>
+    '''
 
 
 @app.route('/unauth_message')
 def unauth_message():
-	return 'Man, did you just try to fuck this site?'
+    return 'Man, did you just try to fuck this site?'
 
 
 @app.route('/protected/')
 @login_required
 def protected():
-	return 'This shit is protected.'
+    return 'This shit is protected.'
 
 
 @app.route('/login/')
 def login():
-	login_user(User(1))
-	return 'Logged In'
+    login_user(User(1))
+    return 'Logged In'
 
 
 @app.route('/logout/')
 def logout():
-	logout_user()
-	return 'Logged Out'
+    logout_user()
+    return 'Logged Out'
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
-
-
-
-
-
-
-
-
-
+    app.run(debug=True)
